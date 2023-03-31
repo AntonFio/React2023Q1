a9d33c71e9317f22680fe72966b4389823e9302e
AntonFio <anton_fiolkovski@mail.ru>
2023-04-01 00:56:46 +0300

    feat: add test

--- react-hook/src/page/Form.tsx
+++ react-hook/src/page/Form.tsx
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/ban-types */
 import './Form.css';
-import React, { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import CartForm from '../components/CartForm';
 
@@ -16,11 +15,11 @@ interface IFormInput {
   valueSelect: GenderEnum;
   valueCheckbox: boolean;
   valueRadio: boolean;
-  images: string;
+  image: string;
 }
 
 const Form = () => {
-  const [item, setItem] = useState([]);
+  const [item, setItem] = useState<IFormInput[]>([]);
   const {
     register,
     handleSubmit,
@@ -31,27 +30,37 @@ const Form = () => {
   const onSubmit = (data: IFormInput) => {
     const value = {
       ...data,
-      image: URL.createObjectURL(data.images[0] as unknown as Blob),
+      image: URL.createObjectURL(data.image[0] as unknown as Blob),
     };
+    setItem([...item, value]);
+    console.log(value.image);
     reset();
   };
-
-  useEffect(() => {
-  
-  }, []);
 
   return (
     <>
       <form className="Form" onSubmit={handleSubmit(onSubmit)}>
         <div>
           <span>Имя:</span>
-          <input {...register('valueName', { required: true })} />
-          <div>{errors?.valueName && <p>This field</p>}</div>
+          <input
+            {...register('valueName', { required: true, pattern: /^(([A-Za-z]|[А-Яа-я]){3,})*$/ })}
+          />
+          <div>
+            {errors?.valueName && (
+              <p style={{ margin: '0', padding: '0 0 10px 0', color: '#af0606' }}>
+                Введите имя(мин 3 буквы)
+              </p>
+            )}
+          </div>
         </div>
         <div>
           <span>Дата:</span>
           <input type="date" {...register('valueDate', { required: true })} />
-          <div>{errors?.valueDate && <p>This field</p>}</div>
+          <div>
+            {errors?.valueDate && (
+              <p style={{ margin: '0', padding: '0 0 10px 0', color: '#af0606' }}>Выбирите дату</p>
+            )}
+          </div>
         </div>
         <div>
           <span>Город:</span>
@@ -60,7 +69,13 @@ const Form = () => {
             <option value="Russia">Russia</option>
             <option value="Ukraine">Ukraine</option>
           </select>
-          <div>{errors?.valueSelect && <p>This field</p>}</div>
+          <div>
+            {errors?.valueSelect && (
+              <p style={{ margin: '0', padding: '0 0 10px 0', color: '#af0606' }}>
+                Выбирите страну
+              </p>
+            )}
+          </div>
         </div>
         <div style={{ display: 'flex' }}>
           <span>согласиться:</span>
@@ -69,7 +84,13 @@ const Form = () => {
             type="checkbox"
             {...register('valueCheckbox', { required: true })}
           />
-          <div>{errors?.valueCheckbox && <p>This field</p>}</div>
+        </div>
+        <div>
+          {errors?.valueCheckbox && (
+            <p style={{ margin: '0', padding: '0 0 10px 0', color: '#af0606' }}>
+              Выбирите согласиться
+            </p>
+          )}
         </div>
         <div style={{ display: 'flex' }}>
           <label className="label-pol">
@@ -89,208 +110,33 @@ const Form = () => {
               {...register('valueRadio', { required: true })}
             />
           </label>
-          <div>{errors?.valueRadio && <p>This field</p>}</div>
         </div>
+        {errors?.valueRadio && (
+          <p style={{ margin: '0', padding: '0 0 10px 0', color: '#af0606' }}>Выбирите пол</p>
+        )}
         <div>
-          <input type="file" accept="image/*" {...register('images', { required: true })} />
+          <input type="file" accept="image/*" {...register('image', { required: true })} />
+          {errors?.image && (
+            <p style={{ margin: '0', padding: '0 0 10px 0', color: '#af0606' }}>
+              Выбирите картинку
+            </p>
+          )}
         </div>
         <div>
           <input className="Btn-form" type="submit" value="Submit" />
         </div>
       </form>
-
-      {/* {data.map((item, index) => (
+      {item.map((item, index) => (
         <CartForm
           key={index}
           name={item.valueName}
           date={item.valueDate}
-          img={URL.createObjectURL(item.images)}
           city={item.valueSelect}
+          img={item.image}
         />
-      ))} */}
+      ))}
     </>
   );
 };
 
 export default Form;
-
-// /* eslint-disable @typescript-eslint/ban-types */
-// import './Form.css';
-// import React, { Component } from 'react';
-// import CartForm from '../components/CartForm';
-
-// interface Prop {
-//   valueName: string;
-//   valueDate: string;
-//   valueSelect: string;
-//   valueCheckbox: boolean;
-//   valueRadio: boolean;
-//   images?: Blob | MediaSource | null;
-// }
-
-// interface State {
-//   data: Prop[];
-//   isValidName: boolean;
-//   isImages: boolean;
-//   block: boolean;
-// }
-
-// export default class Form extends Component<{}, State> {
-//   state = {
-//     data: [] as {
-//       valueName: string;
-//       valueDate: string;
-//       valueSelect: string;
-//       valueCheckbox: boolean;
-//       valueRadio: boolean;
-//       images: Blob | MediaSource;
-//     }[],
-//     isValidName: true,
-//     isImages: true,
-//     block: false,
-//   };
-
-//   inputName = React.createRef<HTMLInputElement>();
-//   inputDate = React.createRef<HTMLInputElement>();
-//   dateSelect = React.createRef<HTMLSelectElement>();
-//   checkbox = React.createRef<HTMLInputElement>();
-//   inputRadio = React.createRef<HTMLInputElement>();
-//   inputFile = React.createRef<HTMLInputElement>();
-//   fileInputRef = React.createRef<HTMLInputElement>();
-
-//   handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     const nameFullReg = /^(([a-zA-Z]|[а-яА-Я]){3,})*$/;
-//     if (!nameFullReg.test(this.inputName.current!.value) || this.inputName.current!.value === '') {
-//       this.setState({ isValidName: false });
-//       return false;
-//     } else if (
-//       !(
-//         this.fileInputRef.current!.files &&
-//         this.fileInputRef.current!.files[0] &&
-//         this.fileInputRef.current!.files.length > 0
-//       )
-//     ) {
-//       this.setState({ isImages: false });
-//       return false;
-//     } else {
-//       const data = {
-//         valueName: this.inputName.current!.value,
-//         valueDate: this.inputDate.current!.value,
-//         valueSelect: this.dateSelect.current!.value,
-//         valueCheckbox: this.checkbox.current!.checked,
-//         valueRadio: this.inputRadio.current!.checked,
-//         images: this.fileInputRef.current?.files && this.fileInputRef.current.files[0],
-//       };
-//       this.setState((prevState) => ({
-//         data: [...prevState.data, data],
-//         isValidName: true,
-//         isImages: true,
-//       }));
-//       this.setState({ block: true });
-//       setTimeout(() => {
-//         this.setState({ block: false });
-//       }, 2000);
-//       e.currentTarget.reset();
-//     }
-//   };
-
-//   render() {
-//     const { data } = this.state;
-//     return (
-//       <>
-//         <form className="Form" onSubmit={this.handleSubmit}>
-//           <div>
-//             <span>Имя:</span>
-//             <input
-//               placeholder="мин 3 буквы"
-//               className={!this.state.isValidName ? 'error-input' : 'error-black'}
-//               type="text"
-//               name="inputName"
-//               ref={this.inputName}
-//               required
-//             />
-//             {!this.state.isValidName ? (
-//               <p style={{ margin: '0', padding: '0', color: 'red' }}>введите корректное имя</p>
-//             ) : (
-//               ''
-//             )}
-//           </div>
-//           <div>
-//             <span>Дата:</span>
-//             <input type="date" name="inputDate" ref={this.inputDate} required />
-//           </div>
-//           <div>
-//             <span>Город:</span>
-//             <select className="Selrct-form" name="selectValue" ref={this.dateSelect} required>
-//               <option>Minsk</option>
-//               <option>Russia</option>
-//               <option>Ukraine</option>
-//             </select>
-//           </div>
-//           <div style={{ display: 'flex' }}>
-//             <span>согласиться:</span>
-//             <input
-//               style={{ width: '4%', margin: '0px' }}
-//               type="checkbox"
-//               name="agree"
-//               ref={this.checkbox}
-//               required
-//             />
-//           </div>
-//           <div style={{ display: 'flex' }}>
-//             <label className="label-pol">
-//               <span>м:</span>
-//               <input
-//                 style={{ width: '100%', marginTop: '5px' }}
-//                 type="radio"
-//                 name="pol"
-//                 ref={this.inputRadio}
-//                 required
-//               />
-//             </label>
-
-//             <label style={{ marginLeft: '10px' }} className="label-pol">
-//               <span>ж: </span>
-//               <input
-//                 style={{ width: '100%', marginTop: '5px' }}
-//                 type="radio"
-//                 name="pol"
-//                 ref={this.inputRadio}
-//                 required
-//               />
-//             </label>
-//           </div>
-//           <div>
-//             <input type="file" accept="image/*" ref={this.fileInputRef} />
-//             {!this.state.isImages ? (
-//               <p style={{ margin: '0', padding: '0', color: 'red' }}>выбери картинку</p>
-//             ) : (
-//               ''
-//             )}
-//           </div>
-//           <div>
-//             <input className="Btn-form" type="submit" value="Submit" />
-//           </div>
-//         </form>
-
-//         {data.map((item, index) => (
-//           <CartForm
-//             key={index}
-//             name={item.valueName}
-//             date={item.valueDate}
-//             img={URL.createObjectURL(item.images)}
-//             city={item.valueSelect}
-//           />
-//         ))}
-//         {this.state.block ? (
-//           <div className="block-ok">
-//             <span>Успешно!</span>
-//           </div>
-//         ) : (
-//           ''
-//         )}
-//       </>
-//     );
-//   }
-// }
