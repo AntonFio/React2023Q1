a25b77d92a15f178c147ca19a37b0cf56c974c08
AntonFio <anton_fiolkovski@mail.ru>
2023-04-04 17:50:18 +0300

    feat: add test

--- react-api/src/page/Home.tsx
+++ react-api/src/page/Home.tsx
@@ -1,12 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Search from '../components/Search/Search';
 import Cart from '../components/Сarts/Cart';
 
-function Home() {
+interface IParam {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: [];
+}
+
+const Home: React.FC = () => {
+  const [param, setParam] = useState<IParam[]>([]);
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then((res) => res.json())
+      .then((data) => setParam(data));
+  }, []);
+
   return (
     <>
       <Search />
-      <h2>Home</h2>
+      <h2>
+        {/* {param.map((item) => {
+          <p key={item.id}>{item.title}</p>;
+        })} */}
+      </h2>
       <Cart />
     </>
   );
