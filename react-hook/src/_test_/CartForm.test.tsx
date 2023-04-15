38a70f8f26ff10e3b38dffe31092fb81faeb50ea
AntonFio <anton_fiolkovski@mail.ru>
2023-04-15 13:42:18 +0300

    feat: add

--- react-hook/src/_test_/CartForm.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import CartForm from '../components/CartForm';
-
-test('render', () => {
-  render(<CartForm name={''} date={''} img={''} city={''} />);
-  const linkElement = screen.getByText(/Город:/i);
-  expect(linkElement).toBeInTheDocument();
-});
