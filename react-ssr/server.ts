b9b3e91c8e000caa7b9b2fbc0e4cee0ffcf4f555
AntonFio <anton_fiolkovski@mail.ru>
2023-04-30 01:45:16 +0300

    feat: add test2

--- react-ssr/server.ts
+++ react-ssr/server.ts
@@ -1,45 +1,44 @@
+import fs from 'fs';
 import path from 'path';
-import express from 'express';
-import { readFile } from 'fs/promises';
 import { fileURLToPath } from 'url';
-import { createServer } from 'vite';
+import express from 'express';
+import { createServer as createViteServer } from 'vite';
 
-const port = process.env.PORT || 3333;
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const app = express();
+async function createServer() {
+  const app = express();
+  const vite = await createViteServer({
+    server: { middlewareMode: true },
+    appType: 'custom',
+  });
+  app.use(vite.middlewares);
 
-const vite = await createServer({
-  server: { middlewareMode: true },
-  appType: 'custom',
-});
-app.use(vite.middlewares);
+  app.use('*', async (req, res, next) => {
+    const url = req.originalUrl;
 
-app.use('*', async (req, res, next) => {
-  const url = req.originalUrl;
-  try {
-    let template = await readFile(path.resolve(__dirname, 'index.html'), 'utf-8');
-    template = await vite.transformIndexHtml(url, template);
+    try {
+      let template = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
+      template = await vite.transformIndexHtml(url, template);
+      const { render } = await vite.ssrLoadModule('/src/entry-server.tsx');
+      const html = template.split(`<!--ssr-outlet-->`);
+      const { pipe } = await render(url, {
+        onShellReady() {
+          res.write(html[0]);
+          pipe(res);
+        },
+        onAllReady() {
+          res.write(html[0] + html[1]);
+          res.end();
+        },
+      });
+    } catch (e) {
+      vite.ssrFixStacktrace(e as Error);
+      next(e as Error);
+    }
+  });
 
-    const { render } = await vite.ssrLoadModule('/src/entry-server.tsx');
-    const pattern = template.split('<!--ssr-body-->');
+  app.listen(3000, () => console.log('http://localhost:3000/'));
+}
 
-    const { pipe } = await render(url, {
-      onShellReady() {
-        res.write(pattern[0]);
-        pipe(res);
-      },
-      onAllReady() {
-        res.write(pattern[1]);
-        res.end();
-      },
-    });
-  } catch (error) {
-    vite.ssrFixStacktrace(error);
-    next(error);
-  }
-});
-
-app.listen(port, () => {
-  console.log(`listening on https://localhost:${port}`);
-});
+createServer();
--- react-ssr/src/entry-client.tsx
+++ react-ssr/src/entry-client.tsx
@@ -1,11 +1,11 @@
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import { store } from './store/store';
 
 ReactDOM.hydrateRoot(
-  document.getElementById('app') as HTMLElement,
+  document.getElementById('root') as HTMLElement,
   <Provider store={store}>
     <BrowserRouter>
       <App />
