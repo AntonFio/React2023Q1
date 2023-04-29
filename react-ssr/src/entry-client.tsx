3094202076b04a40f4be94334f0dcc3b8375cadb
AntonFio <anton_fiolkovski@mail.ru>
2023-04-30 00:40:25 +0300

    feat: add test ssr

---
+++ react-ssr/src/entry-client.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import { store } from './store/store';
+
+ReactDOM.hydrateRoot(
+  document.getElementById('app') as HTMLElement,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
